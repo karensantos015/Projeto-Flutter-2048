571c8b7f2a93a7c4782cefce63bfd5dba827cddc
karensantos015 <kareenribeiroks10@gmail.com>
2025-06-10 18:25:56 -0300

    Add files via upload
    interface 1, ainda estou modificando.

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,247 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
-  runApp(const MyApp());
+  runApp(Jogo2048App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  // This widget is the root of your application.
+class Jogo2048App extends StatelessWidget{
+ const Jogo2048App({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Jogo 2048',
+      home: Jogo2048Screen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class Jogo2048Screen extends StatelessWidget{
+  const Jogo2048Screen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context){
+    return MaterialApp( title: '2048', home: HomePage());
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class HomePage extends StatelessWidget{
+  const HomePage({super.key});
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: Colors.amber.shade50,
+
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              '2048',
+              style: TextStyle(
+                fontFamily: 'Bungee',
+                fontSize: 72,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepOrangeAccent.shade100,
+              ),
             ),
-          ],
+
+
+            const SizedBox(height: 80),
+
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+              criarBotaoNivel(context, 4, Colors.amberAccent.shade200, 'Fácil'),
+              criarBotaoNivel(context, 5, Colors.pink.shade400, 'Médio'),
+              criarBotaoNivel(context, 6, Colors.deepOrangeAccent.shade700, 'Difícil'),
+                         ],
+              ),
+
+        ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget criarBotaoNivel(BuildContext context, int gridSize, Color cor, String nivelTexto){
+    return Padding(
+      padding: EdgeInsets.all(8),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+          backgroundColor: cor,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        onPressed:()=> _iniciarJogo(context, gridSize, nivelTexto),
+        child: Text(
+          nivelTexto,
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.amber.shade50,
+              fontFamily: 'Montserrat',
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+ void _iniciarJogo(BuildContext context, int gridSize, String nivelTexto){
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder:
+        (context)=> PaginaJogo(gridSize:gridSize, nivelTexto:nivelTexto),
+      ),
+    );
+ }
 }
+
+       class PaginaJogo extends StatefulWidget{
+           final int gridSize;
+           final String nivelTexto;
+
+           const PaginaJogo({
+             super.key,
+             required this.gridSize,
+             required this.nivelTexto,
+       });
+           @override
+         State<PaginaJogo> createState() =>_PaginaJogoState();
+       }
+
+       class _PaginaJogoState extends State<PaginaJogo>{
+               Widget criarBotaoMove(BuildContext context, IconData icone){
+                 return Container(
+                   height: 70,
+                   width: 70,
+                   padding: EdgeInsets.all(8),
+                   decoration: BoxDecoration(
+                       color: Colors.black,
+                     border:Border.all(color:  Colors.white, width: 3),
+                     borderRadius: BorderRadius.circular(8),
+                   ),
+                   alignment: Alignment.center,
+                   child: Icon( icone, size: 36, color: Colors.deepOrangeAccent.shade100),
+                 );
+               }
+
+               @override
+               Widget build(BuildContext context) {
+                 return Scaffold(
+                   backgroundColor: Colors.amber.shade50,
+                   appBar: AppBar(
+                     title: Text(
+                       widget.nivelTexto,
+                       style: TextStyle(
+                         color: Colors.blueGrey.shade600,
+                         fontFamily: 'Montserrat.bold',
+                       ),
+                     ),
+                     backgroundColor: Colors.amber.shade50,
+                   ),
+                   body: Padding(
+                     padding: EdgeInsets.all(16),
+                     child: Column(
+                       children: [
+                         Row(
+                           children: [
+                             Expanded(
+                               child: Container(
+                                 height: 60,
+                                 padding: EdgeInsets.all(12),
+                                 decoration: BoxDecoration(
+                                   color: Colors.deepOrange.shade200,
+                                   border: Border.all(
+                                     color: Colors.deepOrange.shade800,
+                                     width: 3,
+                                   ),
+                                   borderRadius: BorderRadius.circular(8),
+                                 ),
+                                 alignment: Alignment.center,
+                                 child: Text(
+                                   'Movimentos: ',
+                                   style: TextStyle(
+                                     fontSize: 24,
+                                     color: Colors.deepOrange.shade800,
+                                     fontFamily: 'Montserrat',
+                                   ),
+                                 ),
+                               ),
+                             ),
+                           ],
+                         ),
+                         SizedBox(height: 80),
+
+                         SizedBox(
+                           height: 500,
+                           child: GridView.builder(
+                             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                               crossAxisCount: widget.gridSize,
+                             ),
+                             itemBuilder:
+                                 (context, index) => Container(
+                               margin: const EdgeInsets.all(2),
+                               decoration: BoxDecoration(
+                                 color: Colors.pink.shade200,
+                                 borderRadius: BorderRadius.circular(8),
+                                 boxShadow: [
+                                   BoxShadow(
+                                     color: Colors.grey.withOpacity(0.3),
+                                     blurRadius: 4,
+                                     offset: Offset(2, 2),
+                                   ),
+                                 ],
+                               ),
+                               child: const Center(
+                                 child: Text(
+                                   '2',
+                                   style: TextStyle(
+                                     color: Colors.pinkAccent,
+                                     fontSize: 24,
+                                     fontFamily: 'RobotoCondensed',
+                                   ),
+                                 ),
+                               ),
+                             ),
+                             itemCount: widget.gridSize * widget.gridSize,
+                           ),
+                         ),
+
+                         SizedBox(height: 20),
+
+                         Row(
+                           mainAxisAlignment: MainAxisAlignment.center,
+                           children: [
+                             criarBotaoMove(context, Icons.arrow_back),
+                             SizedBox(width: 10),
+                             criarBotaoMove(context, Icons.arrow_forward),
+                             SizedBox(width: 10),
+                             criarBotaoMove(context, Icons.arrow_upward),
+                             SizedBox(width: 10),
+                             criarBotaoMove(context, Icons.arrow_downward),
+                           ],
+                         ),
+                       ],
+                     ),
+                   ),
+                 );
+               }
+
+
+
+
+
+
+
+       }
+
